f40551a8f9949c41fb7c4d0edacdc3c55b57b04e
AYP <persianpros@yahoo.com>
2022-09-13 16:00:49 +0430

    Update build.sh

--- CI/build.sh
+++ CI/build.sh
@@ -22,7 +22,7 @@ commit_files() {
 }
 
 upload_files() {
-  git remote add upstream https://${GITHUB_TOKEN}@github.com/OpenVisionE2/ov-bootvideo.git > /dev/null 2>&1
+  git remote add upstream https://${GITHUB_TOKEN}@github.com/OpenVisionE2/ov-logo.git > /dev/null 2>&1
   git push --quiet upstream master || echo "failed to push with error $?"
 }
 
